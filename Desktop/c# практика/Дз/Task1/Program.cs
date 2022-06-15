0d1fccb088b6843f161056bd7a7b3575827cf9de
Sergey <SkyFun1@yandex.ru>
2022-06-15 20:47:25 +0700

    Done 1 task

---
+++ Desktop/c# практика/Дз/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число => ");
+string paramString1 = Console.ReadLine();
+int paramInt1 = int.Parse(paramString1);
+Console.Write("Введите второе число => ");
+string paramString2 = Console.ReadLine();
+int paramInt2 = int.Parse(paramString2);
+if (paramInt1 > paramInt2)
+{
+    Console.WriteLine ("первое число больше второго");
+}
+else
+{
+    Console.WriteLine ("Второе число больше первого");
+}
