a1bf01528c44a91ca9eb94cdd146920552347147
Sergey <SkyFun1@yandex.ru>
2022-06-18 09:05:32 +0700

    Add task 1

---
+++ Desktop/c# практика/дз2/Task1/Program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+//показывает вторую цифру этого числа.
+
+System.Console.Write("Введите трёхзначное число ");
+string number = Console.ReadLine();
+int numberInt = int.Parse(number);
+
+System.Console.WriteLine("Вторая цифра числа "+ (numberInt % 100)/10);
